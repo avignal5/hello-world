24d6079f2517bca46ccedef8e751bc51e7ebf036
Alain Vignal <alain.vignal@inra.fr>
2019-06-19 14:24:41 +0200

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# _*_ coding: Utf-8 _*_
+# coding: utf-8
+
+#CHANGED?
+#NO
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+#from matplotlib import figure as figure
+import re
+import csv
+from collections import defaultdict
